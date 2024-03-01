121f8852ad5366a0d8c155017d81fbedd5ee08d5
sini <sini217@gmail.com>
2024-02-29 20:58:24 -0500

    Added ModulePage. Button connecting Main Page and Modules were added. User Interface for the Modules Page was added and updated.

--- app/src/main/java/com/example/electionapp24/MainActivity.java
+++ app/src/main/java/com/example/electionapp24/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.electionapp24;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Opens the Module Page
+        View MainPageModuleButton = findViewById(R.id.MPModulesButton);
+        MainPageModuleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityModulePage();
+            }
+        });
+    
+    }
+
+    // Function to open Module Page
+    public void openActivityModulePage() {
+        Intent intent = new Intent(this, ModulePage.class);
+        startActivities(new Intent[]{intent});
     }
 }
